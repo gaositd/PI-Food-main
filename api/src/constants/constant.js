69b4a0848cba6cb21cb41bab9e26bf39fae3d5e1
Alvaro Gamez Chavez <gaositd@gmail.com>
2022-04-10 21:47:51 -0500

    paths GETS finished, no tested

---
+++ api/src/constants/constant.js
@@ -0,0 +1,6 @@
+const NOT_FOUND = "Search not found";
+
+
+module.exports ={
+  NOT_FOUND,
+}
--- api/src/routes/controller/recipesController.js
+++ api/src/routes/controller/recipesController.js
@@ -1,11 +1,75 @@
 const dietTypesJSON = require('../../JSON/dietTypesJSON.json');
 const dietType = require('../../../src/db');
+const { default: axios } = require('axios');
+// const fs = require('fs');// save all recipes into JSON file
+// const recipesJson = path.join('../../../src/JSON/recipes.json');
+const { NOT_FOUND } = require('../../constants/constant');
+let resultingRecipes = {}
+
 async function getAllRecipes(req, res){
-  res.send("Hola mundos desde recipes");
+  const {name} = req.query;
+  const { healthScore, title, image, dishTypes, diets, summary, steps } = req.body;
+  try{
+    const allRecipes = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apikey=${process.env.API_KEY}&addRecipeInformation=true`);
+
+    // fs.writeFileSync(recipesJson,allRecipes,{//write in the JSON file
+    //   encoding: "utf8",
+    //   flag: "w+",
+    //   mode: 0o666
+    // });
+
+    const recipesByName = allRecipes.map(recipe => recipe.title.include(name));
+    if(recipes){
+      resultingRecipes = {
+        healthScore,
+        title,
+        image,
+        dishTypes,
+        diets,
+        summary,
+        steps,
+      }
+      res.status(200)
+        .json(resultingRecipes);
+
+    }else{
+      res.status(404)
+        .json({ msg:NOT_FOUND })
+    }
+  }
+  catch(error){
+    return {
+      message: error.message
+    }
+  }
 }
 
 async function getRecipes(req, res){
-  res.sen("Tipos");
+  const {id } = req.params;
+
+  try{
+    const recipe = await axios.get(`https://api.spoonacular.com/recipes/716426/information?apiKey=${process.env.API_KEY}`);
+
+    if(recipe){
+      resultingRecipes = {
+        healthScore,
+        title,
+        image,
+        dishTypes,
+        diets,
+        summary,
+        steps,
+      }
+      res.status(200)
+        .json(resultingRecipes);
+    }else{
+      res.status(404)
+        .json({ msg:NOT_FOUND })
+    }
+
+  }catch(error){
+
+  }
 }
 
 async function putDataInDB(req, res){
@@ -15,6 +79,4 @@ async function putDataInDB(req, res){
 module.exports = {
   getAllRecipes,
   getRecipes,
-  // getDietByType,
-  // putDataInDB
 }
